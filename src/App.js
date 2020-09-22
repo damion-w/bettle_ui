da6d633aa35dac2c9f0e4a02294ae3eed5b38b60
Damion Wiltshire <drw.5452@gmail.com>
2020-09-22 06:24:56 -0700

    fetch from heroku URL in production

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import './App.css';
 function App() {
 
   const [data, setData] = useState(null)
+  let url = ''
+
+  if (process.env.NODE_ENV === 'development') {
+    url = 'http://localhost:3001/'
+  }
+  else {
+    url = 'https://frozen-spire-08641.herokuapp.com/'
+  }
 
   useEffect(() => {
-    fetch("http://localhost:3001/")
-    // fetch("https://frozen-spire-08641.herokuapp.com/")
+    fetch(url)
       .then((res) => res.json())
       .then((res) => {
         console.log(res)
