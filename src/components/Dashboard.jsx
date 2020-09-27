0fcd236c592146b5be615fba37ae97be538f26a3
Damion Wiltshire <drw.5452@gmail.com>
2020-09-27 16:43:13 -0700

    Add styled nav

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,17 +3,23 @@ import Auth from '../modules/Auth'
 import { Redirect } from "react-router-dom";
 import EventList from './EventList';
 import "../../src/App.css";
+import Navigation from './Navigation';
 
 
 const Dashboard = () => {
     return (
-      <div className="dashboard">
-        {!Auth.isUserAuthenticated() && <Redirect to="/" />}
-        <div className="dashboard-title">
-          <h1> Welcome to your dashboard!</h1>
+      <div className="App">
+        <div className="header">
+          <Navigation />
         </div>
-        <div className="event-list">
-          <EventList />
+        <div className="dashboard">
+          {!Auth.isUserAuthenticated() && <Redirect to="/" />}
+          <div className="dashboard-title">
+            <h1> Welcome to your dashboard!</h1>
+          </div>
+          <div className="event-list">
+            <EventList />
+          </div>
         </div>
       </div>
     );
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Auth from '../modules/Auth'
+
+const Navigation = () => {
+
+    function logout() {
+        console.log("LOGOUT!")
+        Auth.deauthenticateUser();
+    }
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Logout</Link>
+        </li>
+
+        <li>
+          <Link to="/">Add a New Event</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation
