6a337cd159509ed82efd5886b3ccf63ac9864e3c
Damion Wiltshire <drw.5452@gmail.com>
2020-09-28 14:58:46 -0700

    delete button working and styled

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,24 +1,52 @@
 import React from "react";import Auth from "../modules/Auth";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 import ExpenseList from './ExpenseList'
 
-const Event = (event) => {
+const Event = ({ name, date, id, expenses, setShouldUpdateEvents }) => {
+  function deleteEvent(evt, eventId) {
+    evt.preventDefault();
+
+    fetch(`${process.env.REACT_APP_API_URI}/events/${eventId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Token: Auth.getToken(),
+        Authorization: `Token ${Auth.getToken()}`,
+      },
+    })
+      .then(() => {
+        setShouldUpdateEvents(true);
+      });
+  }
+
   return (
     <div className="event">
       {!Auth.isUserAuthenticated() && <Redirect to="/" />}
       <div className="event-header">
         <div className="event-title">
-          <p>{event.name}</p>
+          <p>{name}</p>
         </div>
         <div className="date-bubble">
           <div className="event-date">
-            <p>{event.date}</p>
+            <p>{date}</p>
           </div>
         </div>
       </div>
-      <ExpenseList expenses={event.expenses} />
+      <ExpenseList expenses={expenses} />
+
+      <div className="event-footer">
+        <Link to="/eventform">
+          <button className="add-expense-button">New expense</button>
+        </Link>
+        <button
+          className="delete-expense-button"
+          onClick={(evt) => deleteEvent(evt, id)}
+        >
+          Delete event
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Event
